ce2795009e1edd8f7dbb684775c157f9a6f6c58a
Mahesh <maheshveer.skncoe.mech@gmail.com>
2024-03-09 23:28:58 +0530

    All project is completed

--- Library.py
+++ Library.py
@@ -10,7 +10,7 @@ class Library:
             print(book)
 
     def lend_book(self,user,book):
-        if book not in self.lend_dict:
+        if book not in self.lend_dict.keys():
             self.lend_dict.update({book:user})
             print("Database has been succesfully updated. You can take a book now")
         else:
@@ -26,4 +26,35 @@ class Library:
 
 if __name__ == '__main__':
     SKN_Libaray=Library(['Python', 'Rich Daddy Poor Daddy', 'Harry Poter','C++ Basic'], 'Mahesh_Library')
-    
+    print("*"*75)
+    print("""             1. Display All Books
+             2. Lend a Book
+             3. Add New Book
+             4. Return a Book   """)
+    print("*"*75)  
+    while(True):
+        choise = input("Enter a choise to perfrom a Operation: ")
+        if choise == '1':
+            SKN_Libaray.display_book()
+        elif choise == '2':
+            user = input("Enter Your Name: ")
+            book = input("Enter book name to lend: ")
+            SKN_Libaray.lend_book(user,book)
+        elif choise == '3':
+            book = input("Enter a book name to add: ")
+            SKN_Libaray.add_book(book)
+        elif choise == '4':
+            book = input("Enter a book to return: ")
+            SKN_Libaray.return_book(book)
+        else:
+            print("Please enter a valid option.")
+
+        print(" Press q to Quit and c to Continue: ")
+        user_choise = input()
+        while(user_choise != "c" and user_choise != "q"):
+            if user_choise == "q":
+                exit()
+            elif user_choise == "c":
+                continue
+            else:
+                print("Enter a Valid Option") 
